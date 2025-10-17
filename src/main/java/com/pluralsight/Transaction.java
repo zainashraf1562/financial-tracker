5c40a5c016faf585fe6eb4ee43d9a2c2ed48c126
Zain Ashraf <zainashraf1562@gmail.com>
2025-10-17 04:06:26 -0400

    Made Final Transaction changes

--- src/main/java/com/pluralsight/Transaction.java
+++ src/main/java/com/pluralsight/Transaction.java
@@ -22,50 +22,24 @@ public class Transaction {
         return date;
     }
 
-    public void setDate(LocalDate date){
-        this.date = date;
-    }
-
     public LocalTime getTime(){
         return time;
-    }
-
-    public void setTime(LocalTime time){
-        this.time = time;
     }
 
     public String getDescription(){
         return description;
     }
 
-    public void setDescription(String description){
-        this.description = description;
-    }
-
     public String getVendor(){
         return vendor;
-    }
-
-    public void setVendor(String vendor){
-        this.vendor = vendor;
     }
 
     public double getAmount(){
         return amount;
     }
 
-    public void setAmount(double amount){
-        this.amount = amount;
-    }
-
     @Override
     public String toString() {
-        return "Transaction: {"+
-                "date: " + getDate() +
-                ", time: " + getTime() +
-                ", description: " + getDescription()  +
-                ", vendor: " + getVendor()  +
-                ", amount: " + getAmount() +
-                '}' + "\n";
+        return "Transaction: " + getDate() + getTime() + getDescription()  + getVendor()  + getAmount();
     }
 }
